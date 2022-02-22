c444b8b65757abb9425b90642b373f409307b117
Siddharth Mishra-Sharma <smsharma@mit.edu>
2022-02-22 18:53:45 -0500

    Glow is working well; fuck yes

---
+++ models/flow_utils.py
@@ -0,0 +1,264 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+def create_checkerboard_mask(h, w, invert=False):
+    x, y = torch.arange(h, dtype=torch.int32), torch.arange(w, dtype=torch.int32)
+    xx, yy = torch.meshgrid(x, y)
+    mask = torch.fmod(xx + yy, 2)
+    mask = mask.to(torch.float32).view(1, 1, h, w)
+    if invert:
+        mask = 1 - mask
+    return mask
+
+def create_channel_mask(c_in, invert=False):
+    mask = torch.cat([torch.ones(c_in//2, dtype=torch.float32),
+                      torch.zeros(c_in-c_in//2, dtype=torch.float32)])
+    mask = mask.view(1, c_in, 1, 1)
+    if invert:
+        mask = 1 - mask
+    return mask
+
+class CouplingLayer(nn.Module):
+
+    def __init__(self, network, mask, c_in):
+        """
+        Coupling layer inside a normalizing flow.
+        Inputs:
+            network - A PyTorch nn.Module constituting the deep neural network for mu and sigma.
+                      Output shape should be twice the channel size as the input.
+            mask - Binary mask (0 or 1) where 0 denotes that the element should be transformed,
+                   while 1 means the latent will be used as input to the NN.
+            c_in - Number of input channels
+        """
+        super().__init__()
+        self.network = network
+        self.scaling_factor = nn.Parameter(torch.zeros(c_in))
+        # Register mask as buffer as it is a tensor which is not a parameter,
+        # but should be part of the modules state.
+        self.register_buffer('mask', mask)
+
+    def forward(self, z, ldj, reverse=False, orig_img=None):
+        """
+        Inputs:
+            z - Latent input to the flow
+            ldj - The current ldj of the previous flows.
+                  The ldj of this layer will be added to this tensor.
+            reverse - If True, we apply the inverse of the layer.
+            orig_img (optional) - Only needed in VarDeq. Allows external
+                                  input to condition the flow on (e.g. original image)
+        """
+        # Apply network to masked input
+        z_in = z * self.mask
+        if orig_img is None:
+            nn_out = self.network(z_in)
+        else:
+            nn_out = self.network(torch.cat([z_in, orig_img], dim=1))
+        s, t = nn_out.chunk(2, dim=1)
+
+        # Stabilize scaling output
+        s_fac = self.scaling_factor.exp().view(1, -1, 1, 1)
+        s = torch.tanh(s / s_fac) * s_fac
+
+        # Mask outputs (only transform the second part)
+        s = s * (1 - self.mask)
+        t = t * (1 - self.mask)
+
+        # Affine transformation
+        if not reverse:
+            # Whether we first shift and then scale, or the other way round,
+            # is a design choice, and usually does not have a big impact
+            z = (z + t) * torch.exp(s)
+            ldj += s.sum(dim=[1,2,3])
+        else:
+            z = (z * torch.exp(-s)) - t
+            ldj -= s.sum(dim=[1,2,3])
+
+        return z, ldj
+
+class ConcatELU(nn.Module):
+    """
+    Activation function that applies ELU in both direction (inverted and plain).
+    Allows non-linearity while providing strong gradients for any input (important for final convolution)
+    """
+
+    def forward(self, x):
+        return torch.cat([F.elu(x), F.elu(-x)], dim=1)
+
+
+class LayerNormChannels(nn.Module):
+
+    def __init__(self, c_in):
+        """
+        This module applies layer norm across channels in an image. Has been shown to work well with ResNet connections.
+        Inputs:
+            c_in - Number of channels of the input
+        """
+        super().__init__()
+        self.layer_norm = nn.LayerNorm(c_in)
+
+    def forward(self, x):
+        x = x.permute(0, 2, 3, 1)
+        x = self.layer_norm(x)
+        x = x.permute(0, 3, 1, 2)
+        return x
+
+
+class GatedConv(nn.Module):
+
+    def __init__(self, c_in, c_hidden):
+        """
+        This module applies a two-layer convolutional ResNet block with input gate
+        Inputs:
+            c_in - Number of channels of the input
+            c_hidden - Number of hidden dimensions we want to model (usually similar to c_in)
+        """
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(c_in, c_hidden, kernel_size=3, padding=1),
+            ConcatELU(),
+            nn.Conv2d(2*c_hidden, 2*c_in, kernel_size=1)
+        )
+
+    def forward(self, x):
+        out = self.net(x)
+        val, gate = out.chunk(2, dim=1)
+        return x + val * torch.sigmoid(gate)
+
+
+class GatedConvNet(nn.Module):
+
+    def __init__(self, c_in, c_hidden=32, c_out=-1, num_layers=3):
+        """
+        Module that summarizes the previous blocks to a full convolutional neural network.
+        Inputs:
+            c_in - Number of input channels
+            c_hidden - Number of hidden dimensions to use within the network
+            c_out - Number of output channels. If -1, 2 times the input channels are used (affine coupling)
+            num_layers - Number of gated ResNet blocks to apply
+        """
+        super().__init__()
+        c_out = c_out if c_out > 0 else 2 * c_in
+        layers = []
+        layers += [nn.Conv2d(c_in, c_hidden, kernel_size=3, padding=1)]
+        for layer_index in range(num_layers):
+            layers += [GatedConv(c_hidden, c_hidden),
+                       LayerNormChannels(c_hidden)]
+        layers += [ConcatELU(),
+                   nn.Conv2d(2*c_hidden, c_out, kernel_size=3, padding=1)]
+        self.nn = nn.Sequential(*layers)
+
+        self.nn[-1].weight.data.zero_()
+        self.nn[-1].bias.data.zero_()
+
+    def forward(self, x):
+        return self.nn(x)
+
+class SplitFlow(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.prior = torch.distributions.normal.Normal(loc=0.0, scale=1.0)
+
+    def forward(self, z, ldj, reverse=False):
+        if not reverse:
+            z, z_split = z.chunk(2, dim=1)
+            ldj += self.prior.log_prob(z_split).sum(dim=[1,2,3])
+        else:
+            z_split = self.prior.sample(sample_shape=z.shape).to(device)
+            z = torch.cat([z, z_split], dim=1)
+            ldj -= self.prior.log_prob(z_split).sum(dim=[1,2,3])
+        return z, ldj
+
+class SqueezeFlow(nn.Module):
+
+    def forward(self, z, ldj, reverse=False):
+        B, C, H, W = z.shape
+        if not reverse:
+            # Forward direction: H x W x C => H/2 x W/2 x 4C
+            z = z.reshape(B, C, H//2, 2, W//2, 2)
+            z = z.permute(0, 1, 3, 5, 2, 4)
+            z = z.reshape(B, 4*C, H//2, W//2)
+        else:
+            # Reverse direction: H/2 x W/2 x 4C => H x W x C
+            z = z.reshape(B, C//4, 2, 2, H, W)
+            z = z.permute(0, 1, 4, 2, 5, 3)
+            z = z.reshape(B, C//4, H*2, W*2)
+        return z, ldj
+
+class Dequantization(nn.Module):
+
+    def __init__(self, alpha=1e-5, quants=300):
+        """
+        Inputs:
+            alpha - small constant that is used to scale the original input.
+                    Prevents dealing with values very close to 0 and 1 when inverting the sigmoid
+            quants - Number of possible discrete values (usually 256 for 8-bit image)
+        """
+        super().__init__()
+        self.alpha = alpha
+        self.quants = quants
+
+    def forward(self, z, ldj, reverse=False):
+        if not reverse:
+            z, ldj = self.dequant(z, ldj)
+            z, ldj = self.sigmoid(z, ldj, reverse=True)
+        else:
+            z, ldj = self.sigmoid(z, ldj, reverse=False)
+            z = z * self.quants
+            ldj += np.log(self.quants) * np.prod(z.shape[1:])
+            z = torch.floor(z).clamp(min=0, max=self.quants-1).to(torch.int32)
+        return z, ldj
+
+    def sigmoid(self, z, ldj, reverse=False):
+        # Applies an invertible sigmoid transformation
+        if not reverse:
+            ldj += (-z-2*F.softplus(-z)).sum(dim=[1,2,3])
+            z = torch.sigmoid(z)
+        else:
+            z = z * (1 - self.alpha) + 0.5 * self.alpha  # Scale to prevent boundaries 0 and 1
+            ldj += np.log(1 - self.alpha) * np.prod(z.shape[1:])
+            ldj += (-torch.log(z) - torch.log(1-z)).sum(dim=[1,2,3])
+            z = torch.log(z) - torch.log(1-z)
+        return z, ldj
+
+    def dequant(self, z, ldj):
+        # Transform discrete values to continuous volumes
+        z = z.to(torch.float32)
+        z = z + torch.rand_like(z).detach()
+        z = z / self.quants
+        ldj -= np.log(self.quants) * np.prod(z.shape[1:])
+        return z, ldj
+
+class VariationalDequantization(Dequantization):
+
+    def __init__(self, var_flows, alpha=1e-5, quants=300):
+        """
+        Inputs:
+            var_flows - A list of flow transformations to use for modeling q(u|x)
+            alpha - Small constant, see Dequantization for details
+        """
+        super().__init__(alpha=alpha)
+        self.flows = nn.ModuleList(var_flows)
+        self.quants = quants
+
+    def dequant(self, z, ldj):
+        z = z.to(torch.float32)
+        img = (z / (self.quants - 1)) * 2 - 1 # We condition the flows on x, i.e. the original image
+
+        # Prior of u is a uniform distribution as before
+        # As most flow transformations are defined on [-infinity,+infinity], we apply an inverse sigmoid first.
+        deq_noise = torch.rand_like(z).detach()
+        deq_noise, ldj = self.sigmoid(deq_noise, ldj, reverse=True)
+        for flow in self.flows:
+            deq_noise, ldj = flow(deq_noise, ldj, reverse=False, orig_img=img)
+        deq_noise, ldj = self.sigmoid(deq_noise, ldj, reverse=False)
+
+        # After the flows, apply u as in standard dequantization
+        z = (z + deq_noise) / self.quants
+        ldj -= np.log(self.quants) * np.prod(z.shape[1:])
+        return z, ldj
