9bb2c728a9000c8d989e8e8e42a99492630c8efe
Yitian Sun <yitian.sun.26@gmail.com>
2023-08-23 16:19:15 -0400

    hmc tests

---
+++ validating/svi_confidence/submit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=hmc_array
+#SBATCH --partition=iaifi_gpu_mig
+#SBATCH --array=0-29
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=16GB
+#SBATCH --gres=gpu:1
+#SBATCH --time=0-08:00:00
+#SBATCH --output=/n/home07/yitians/fermi/fermi-prob-prog/outputs/slurm/%x_%j.out
+#SBATCH --error=/n/home07/yitians/fermi/fermi-prob-prog/outputs/slurm/%x_%j.err
+#SBATCH --account=iaifi_lab
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=yitians@mit.com
+
+source /n/home07/yitians/setup_torch.sh
+
+cd /n/home07/yitians/fermi/fermi-prob-prog/validating/svi_confidence
+
+srun python run_hmc.py -i $SLURM_ARRAY_TASK_ID --use_neutra
