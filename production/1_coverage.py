fb9891ae990e90b3048d48b91ec204023ed5f6c9
Yitian Sun <yitian.sun.26@gmail.com>
2024-08-03 06:38:51 -0400

    save coverage code

--- production/1_coverage.py
+++ production/1_coverage.py
@@ -13,7 +13,7 @@ from utils.validation import find_hdi_prob
 if __name__ == '__main__':
 
     samples_dir = f"{wdir}/../outputs/fit/svi_240803"
-    theta_true = json.load(open(f"{wdir}/theta_true_flat.json"))
+    theta_true = json.load(open(f"{wdir}/truth_dict_flat.json"))
 
     ks = [
         "S_bub", "S_gce", "S_ics", "S_iso", "S_pib", "S_psc",
@@ -24,20 +24,22 @@ if __name__ == '__main__':
     ]
     n_sim = 30
 
-    prob_samples = {}
+    samples_list = []
     for i in tqdm(range(n_sim)):
-        samples = pickle.load(open(f"{samples_dir}/svi_samples_i{i}_n50000.p", 'rb'))
+        samples_list.append(pickle.load(open(f"{samples_dir}/svi_samples_i{i}_n50000.p", 'rb')))
+    
+    prob_samples = {}
+    for k in tqdm(ks):
         probs = []
-        for k in ks:
-            samples_test = samples[k]
+        for i in range(n_sim):
+            samples_test = np.array(samples_list[i][k])
             truth_test = theta_true[k]
             probs.append(find_hdi_prob(samples_test, truth_test))
         prob_samples[k] = np.array(probs)
-    prob_samples = np.array(prob_samples).T
 
     p_nominal_actual_dict = {}
     for k in ks:
-        p_nominal, p_actual = np.sort(prob_samples[k]), np.linspace(0, 1, len(prob_samples[i]))
+        p_nominal, p_actual = np.sort(prob_samples[k]), np.linspace(0, 1, len(prob_samples[k]))
         p_nominal_actual_dict[k] = (p_nominal, p_actual)
 
     pickle.dump(p_nominal_actual_dict, open(f"{samples_dir}/p_nominal_actual_dict.p", 'wb'))
