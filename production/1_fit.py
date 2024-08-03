5c3be9b772000e40906528c398608f08fd7d9d5f
Yitian Sun <yitian.sun.26@gmail.com>
2024-08-03 00:34:28 -0400

    fit

---
+++ production/1_fit.py
@@ -0,0 +1,60 @@
+import sys
+import dill as pickle
+import argparse
+
+import numpy as np
+import healpy as hp
+
+import jax.numpy as jnp
+
+wdir = "/n/home07/yitians/fermi/fermi-prob-prog/production"
+sys.path.append(f"{wdir}/..")
+from models.np_model import NPModel
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', type=int, default=0)
+    parser.add_argument('-n', type=int, default=10000)
+    parser.add_argument('--n_step', type=int, default=2000)
+    parser.add_argument('--fit_type', type=str, default='test')
+    args = parser.parse_args()
+
+    wdir = "/n/home07/yitians/fermi/fermi-prob-prog/production"
+    data_dir = f"{wdir}/../data/fermi_data_573w/fermi_data_128"
+    save_dir = f"{wdir}/../outputs/fit/svi_240803"
+
+    mask_roi = jnp.load(f"{wdir}/mask_roi.npy")
+    mask_norm = jnp.load(f"{wdir}/mask_norm.npy")
+
+    data = np.load(f"{wdir}/sim_truth_n30.npy")[args.i]
+    data_full = np.zeros(hp.nside2npix(128))
+    data_full[~mask_norm] = data
+    data_in = jnp.array(data_full, dtype=jnp.int32)
+
+
+    m = NPModel()
+
+    if args.fit_type == 'svi':
+        m.fit_svi(n_steps=args.n_step, data=data_in)
+        samples = m.get_svi_samples(num_samples=args.n)
+
+    elif args.fit_type in ['hmc', 'hmcnt']:
+        mcmc = m.run_nuts(
+            num_chains=4, num_warmup=500, num_samples=args.n//4,
+            use_neutra=(args.fit_type=='hmcnt'), data=data_in
+        )
+        samples = mcmc.get_samples()
+
+    elif args.fit_type == 'test':
+        mcmc = m.run_nuts(
+            num_chains=4, num_warmup=10, num_samples=10,
+            use_neutra=False, data=data_in
+        )
+        samples = mcmc.get_samples()
+
+    else:
+        raise NotImplementedError(args.fit_type)
+    
+    pickle.dump(samples, open(f"{save_dir}/{args.fit_type}_samples_i{args.i}_n{args.n}.p", 'wb'))
---
+++ production/submit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=fpp_svi
+#SBATCH --array=0-29
+#SBATCH --partition=iaifi_gpu
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64GB
+#SBATCH --time=0-08:00:00
+#SBATCH --output=/n/home07/yitians/fermi/fermi-prob-prog/outputs/slurm/%x_%a.out
+#SBATCH --error=/n/home07/yitians/fermi/fermi-prob-prog/outputs/slurm/%x_%a.err
+#SBATCH --account=iaifi_lab
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=yitians@mit.com
+
+#source /n/home07/yitians/setup/jax.sh
+source /n/home07/yitians/setup/torch.sh
+
+cd /n/home07/yitians/fermi/fermi-prob-prog/production
+
+python 1_fit.py -i $SLURM_ARRAY_TASK_ID -n 50000 --fit_type svi --n_step 2000
