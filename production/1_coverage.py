dd2b12063e19e09b7d3b0b0779f717219817f550
Yitian Sun <yitian.sun.26@gmail.com>
2024-08-03 17:59:00 +0800

    coverage

--- production/1_coverage.py
+++ production/1_coverage.py
@@ -7,15 +7,13 @@ import numpy as np
 
 wdir = "/n/home07/yitians/fermi/fermi-prob-prog/production"
 sys.path.append(f"{wdir}/..")
-
-samples_dir = f"{wdir}/../outputs/fit/svi_240803"
-
+from utils.validation import find_hdi_prob
 
 
 if __name__ == '__main__':
 
-    config = json.load(open(f"{wdir}/config.json"))
-    theta_true = config["theta_true"]
+    samples_dir = f"{wdir}/../outputs/fit/svi_240803"
+    theta_true = json.load(open(f"{wdir}/theta_true_flat.json"))
 
     ks = [
         "S_bub", "S_gce", "S_ics", "S_iso", "S_pib", "S_psc",
@@ -28,20 +26,18 @@ if __name__ == '__main__':
 
     prob_samples = {}
     for i in tqdm(range(n_sim)):
-        
         samples = pickle.load(open(f"{samples_dir}/svi_samples_i{i}_n50000.p", 'rb'))
-
         probs = []
         for k in ks:
             samples_test = samples[k]
-            truth_test = theta_true[i]
-            probs.append(find_sample_hdi_prob(samples_test, truth_test))
+            truth_test = theta_true[k]
+            probs.append(find_hdi_prob(samples_test, truth_test))
         prob_samples[k] = np.array(probs)
     prob_samples = np.array(prob_samples).T
 
     p_nominal_s = []
     p_actual_s = []
-    for i in range(cd.thetas_mean.shape[0]):
-        p_nominal, p_actual = np.sort(prob_samples[i]), np.linspace(0, 1, len(prob_samples[i]))
+    for k in ks:
+        p_nominal, p_actual = np.sort(prob_samples[k]), np.linspace(0, 1, len(prob_samples[i]))
         p_nominal_s.append(p_nominal)
         p_actual_s.append(p_actual)
