b6515ea3a6a7638f9f8ad688302dbb619afd746d
Yitian Sun <yitian.sun.26@gmail.com>
2024-11-07 17:32:48 -0500

    pthmc_Dbase23fix_deltapsf_Mbase23fix_deltapsf results

--- production/1_coverage.py
+++ production/1_coverage.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     truth_name = 'base230927'
     data_name = 'base23fix_deltapsf'
     model_name = 'base23fix_deltapsf'
-    run_name = f'hmc_Dbase23fix_deltapsf_Mbase23fix_deltapsf'
+    run_name = f'pthmc_Dbase23fix_deltapsf_Mbase23fix_deltapsf'
     print(f"Run name: {run_name}")
 
     samples_dir = f"{wdir}/../outputs/fit/{run_name}"
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     samples_list = []
     missing_list = []
     for i in tqdm(range(n_sim)):
-        fn = f"{samples_dir}/i{i}_n10000_ns10000.p"
+        fn = f"{samples_dir}/i{i}_n10000_ns2000.p"
         if not os.path.exists(fn):
             missing_list.append(i)
         else:
