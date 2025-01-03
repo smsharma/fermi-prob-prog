826ef9bfc3291c139a5ba8e1796040e1a35bdaf6
Yitian Sun <yitian.sun.26@gmail.com>
2025-01-03 17:14:08 -0500

    hmc_Dbase23fix_smalldsk_deltapsf_Mbase23fix_deltapsf_n20000 and svi_Dbase23fix_smalldsk_deltapsf_Mbase23fix_deltapsf_ns30000

--- production/submit.sh
+++ production/submit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=svi_Dbase23fix_deltapsf_Mbase23fix_deltapsf
-#SBATCH --array=0-29
-#SBATCH --partition=gpu
+#SBATCH --job-name=hmc_Dbase23fix_smalldsk_deltapsf_Mbase23fix_deltapsf_n20000
+#SBATCH --array=0-0
+#SBATCH --partition=iaifi_gpu
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
@@ -18,9 +18,11 @@ source /n/home07/yitians/setup/torch.sh
 
 cd /n/home07/yitians/fermi/fermi-prob-prog/production
 
-python 1_fit.py -i $SLURM_ARRAY_TASK_ID --fit_type svi --n_step 10000 -n 50000 --data base23fix_deltapsf --model base23fix_deltapsf
-# python 1_fit.py -i $SLURM_ARRAY_TASK_ID --fit_type hmc -n 10000 --n_step 0 --data base23fix_smalldsk_deltapsf --model base23fix_deltapsf
+# python 1_fit.py -i $SLURM_ARRAY_TASK_ID --fit_type svi --n_step 30000 -n 50000 --data base23fix_smalldsk_deltapsf --model base23fix_deltapsf
+python 1_fit.py -i $SLURM_ARRAY_TASK_ID --fit_type hmc -n 20000 --n_step 0 --data base23fix_smalldsk_deltapsf --model base23fix_deltapsf
 
 # run svi: python 1_fit.py -i 0 --fit_type svi -n 10000 --n_step 100 --data base23fix_smalldsk_deltapsf --model base23fix_deltapsf --comment test
 # test hmc: python 1_fit.py -i 0 --fit_type testhmc --data base23fix_smalldsk --model base23fix --comment test
 # test pthmc: python 1_fit.py -i 0 --fit_type pthmc -n 10000 --n_step 2000 --data psc_deltapsf --model base23fix_deltapsf
+
+svi_Dbase23fix_smalldsk_deltapsf_Mbase23fix_deltapsf_ns30000
