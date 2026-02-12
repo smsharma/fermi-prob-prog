normalization bug

analysis/fit_cmp.py
@@ -19,6 +19,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--run_name', type=str)
     parser.add_argument('--fit_type', type=str)
+    parser.add_argument('--model', type=str, default='model')
+    parser.add_argument('--data', type=str)
     parser.add_argument('-n', type=int)
     parser.add_argument('--n_step', type=int, default=0)
     parser.add_argument('--lr', type=float, default=3e-4)
@@ -26,20 +28,21 @@ if __name__ == '__main__':
 
     run_name = args.run_name
     print('run_name:', run_name)
-
+    print('model:', args.model)
     save_dir = f"{wdir}/../outputs/fits_cmp/{run_name}"
     os.makedirs(save_dir, exist_ok=True)
 
-    #===== model =====
-    m = NPModelCMP()
+    #===== data & model =====
+    data = jnp.array(np.load(f'/n/home07/yitians/fermi/fermi-prob-prog/outputs/simulations/{args.data}.npy').astype(np.int32)[0])
+    m = NPModelCMP(data=data)
 
     #===== fit =====
     if args.fit_type == 'svi':
-        m.fit_svi(n_steps=args.n_step, lr=args.lr)
+        m.fit_svi(model_name=args.model, n_steps=args.n_step, lr=args.lr)
         samples = m.get_svi_samples(num_samples=args.n)
 
     elif args.fit_type == 'hmc':
-        mcmc = m.run_nuts(num_chains=4, num_warmup=1000, num_samples=args.n//4, step_size=0.05,)
+        mcmc = m.run_nuts(model_name=args.model, num_chains=4, num_warmup=1000, num_samples=args.n//4, step_size=0.05,)
         samples = mcmc.get_samples()
 
     else:

analysis/simulate.py
@@ -3,25 +3,30 @@ import healpy as hp
 import json
 from tqdm import tqdm
 
-from fpp.models.np_model import NPModel
-from fpp.simulations.wrapper import simulator_for_model, simulator_for_model_p6v11
+# from fpp.models.np_model import NPModel
+from fpp.models.np_model_cmp import NPModelCMP
+# from fpp.simulations.wrapper import simulator_for_model, simulator_for_model_p6v11
+from fpp.simulations.wrapper import simulator_for_cmp
 
 
 if __name__ == '__main__':
 
-    data_name = 'base23fix_p6v11'
+    data_name = 'cmp'
+    truth_name = 'cmp'
     n_sim = 30
     delta_psf = False
     flat_exposure = False
-    sim_func = simulator_for_model_p6v11
+    sim1b = True
+    sim_func = simulator_for_cmp
     
-    truth_dict = json.load(open("../outputs/truths/truth_dict_base230927_p6v11.json", "r"))
-    m = NPModel(data=np.zeros(hp.nside2npix(128), dtype=np.int32)) # dummy data
+    truth_dict = json.load(open(f"../outputs/truths/truth_dict_{truth_name}.json", "r"))
+    # m = NPModel(data=np.zeros(hp.nside2npix(128), dtype=np.int32)) # dummy data
+    m = NPModelCMP()
     # m.debug_exaggerate_exposure(5)
 
     sims = []
     for _ in tqdm(range(n_sim)):
-        sims.append(sim_func(m, truth_dict, sim_all=True, delta_psf=delta_psf, flat_exposure=flat_exposure))
+        sims.append(sim_func(m, truth_dict, sim_all=True, delta_psf=delta_psf, flat_exposure=flat_exposure, sim1b=sim1b))
     sims = np.array(sims)
 
     np.save(f"../outputs/simulations/{data_name}.npy", sims)

src/fpp/models/np_model.py
@@ -319,11 +319,17 @@ class NPModel:
         self.temp_mF_ics = np.load("{}/template_Fic.npy".format(self.data_dir))
 
         # Normalize all templates
-        for t in [self.temp_psc, self.temp_iso, self.temp_bub, self.temp_dsk, self.temp_p6v11,
-                  self.temp_mO_pib, self.temp_mO_ics,
-                  self.temp_mA_pib, self.temp_mA_ics,
-                  self.temp_mF_pib, self.temp_mF_ics]:
-            t /= np.mean(t[~self.normalization_mask])
+        self.temp_psc /= np.mean(self.temp_psc[~self.normalization_mask])
+        self.temp_iso /= np.mean(self.temp_iso[~self.normalization_mask])
+        self.temp_bub /= np.mean(self.temp_bub[~self.normalization_mask])
+        self.temp_dsk /= np.mean(self.temp_dsk[~self.normalization_mask])
+        self.temp_p6v11 /= np.mean(self.temp_p6v11[~self.normalization_mask])
+        self.temp_mO_pib /= np.mean(self.temp_mO_pib[~self.normalization_mask])
+        self.temp_mO_ics /= np.mean(self.temp_mO_ics[~self.normalization_mask])
+        self.temp_mA_pib /= np.mean(self.temp_mA_pib[~self.normalization_mask])
+        self.temp_mA_ics /= np.mean(self.temp_mA_ics[~self.normalization_mask])
+        self.temp_mF_pib /= np.mean(self.temp_mF_pib[~self.normalization_mask])
+        self.temp_mF_ics /= np.mean(self.temp_mF_ics[~self.normalization_mask])
                 
         self.pib = []
         self.ics = []
@@ -396,8 +402,10 @@ class NPModel:
             gamma_ps = 1.2 if self.non_poissonian else None
             gamma_poiss = 1.2
 
-        temp_gce_nfw_ps = self.nfw_template.get_NFW2_template(gamma=gamma_ps) if self.non_poissonian else None
+        temp_gce_nfw_ps = self.nfw_template.get_NFW2_template(gamma=gamma_ps)
         temp_gce_nfw_poiss = self.nfw_template.get_NFW2_template(gamma=gamma_poiss)
+        temp_gce_nfw_ps /= jnp.mean(temp_gce_nfw_ps[~self.normalization_mask])
+        temp_gce_nfw_poiss /= jnp.mean(temp_gce_nfw_poiss[~self.normalization_mask])
             
         if self.non_poissonian:
             if self.vary_disk:
@@ -406,6 +414,7 @@ class NPModel:
                 temp_dsk = self.disk_template.get_template(zs=zs, C=C)
             else:
                 temp_dsk = self.temp_dsk
+            temp_dsk /= jnp.mean(temp_dsk[~self.normalization_mask])
                 
         if self.bulge_hybrid:
             f_bulge_ps = numpyro.sample("f_bulge_ps", dist.Uniform(0., 1.)) if self.non_poissonian else None

src/fpp/models/np_model_cmp.py
@@ -27,13 +27,16 @@ from fpp.utils.psf_correction import PSFCorrection
 class NPModelCMP:
 
 
-    def __init__(self):
+    def __init__(self, data=None):
 
         self.n_exp = 7
         self.nside = 128
         self.data_dir = "/n/home07/yitians/fermi/fermi-prob-prog/data/fermi_data_573w/fermi_data_128/"
 
-        self.data = jnp.array(np.load(f"{self.data_dir}/fermidata_counts.npy").astype(np.int32))
+        if data is None:
+            self.data = jnp.array(np.load(f"{self.data_dir}/fermidata_counts.npy").astype(np.int32))
+        else:
+            self.data = jnp.array(data.astype(np.int32))
         self.exposure_map = jnp.array(np.load(f"{self.data_dir}/fermidata_exposure.npy"))
 
         mask_ps = hp.ud_grade(np.load(f"{self.data_dir}/../../mask_3fgl_0p8deg.npy"), nside_out=self.nside) > 0
@@ -51,9 +54,13 @@ class NPModelCMP:
         self.dsk = jnp.array(np.load(f"{self.data_dir}/template_dsk_z0p3.npy"))
         self.nfw_1p0 = jnp.array(np.load(f"{self.data_dir}/template_nfw_g1p0.npy"))
         self.nfw_1p2 = jnp.array(np.load(f"{self.data_dir}/template_nfw_g1p2.npy"))
-        self.blg = jnp.array(np.load(f"{self.data_dir}/../../nptfit_cmp/coleman2019_bulge_template.npy"))
-        for t in [self.pib, self.ics, self.psc, self.bub, self.iso, self.dsk, self.nfw_1p0, self.nfw_1p2, self.blg]:
-            t /= jnp.mean(t[~self.normalization_mask])
+        self.pib = self.pib / jnp.mean(self.pib[~self.normalization_mask])
+        self.ics = self.ics / jnp.mean(self.ics[~self.normalization_mask])
+        self.psc = self.psc / jnp.mean(self.psc[~self.normalization_mask])
+        self.bub = self.bub / jnp.mean(self.bub[~self.normalization_mask])
+        self.iso = self.iso / jnp.mean(self.iso[~self.normalization_mask])
+        self.nfw_1p0 = self.nfw_1p0 / jnp.mean(self.nfw_1p0[~self.normalization_mask])
+        self.nfw_1p2 = self.nfw_1p2 / jnp.mean(self.nfw_1p2[~self.normalization_mask])
 
         self.get_psf_correction()
         self.get_exp_regions(self.n_exp)
@@ -127,22 +134,16 @@ class NPModelCMP:
         mu += numpyro.sample(f'S_bub', dist.Uniform(PRIOR_LOW, PRIOR_HIGH)) * self.bub
         mu += numpyro.sample(f'S_psc', dist.Uniform(PRIOR_LOW, PRIOR_HIGH)) * self.psc
         mu += numpyro.sample(f'S_nfw', dist.Uniform(PRIOR_LOW, PRIOR_HIGH)) * self.nfw_1p0
-        # mu += numpyro.sample(f'S_blg', dist.Uniform(PRIOR_LOW, PRIOR_HIGH)) * self.blg
 
         # np templates
         npt_compressed = jnp.array([self.nfw_1p2, self.dsk])
         theta = []
         for ips, ps in enumerate(["nfw", "dsk"]):
-            Sps = numpyro.sample(f"Sps_{ps}", dist.Uniform(PRIOR_LOW, PRIOR_HIGH))
-            n1  = numpyro.sample(f"n1_{ps}",  dist.Uniform(4.0, 6.0))
-            n2  = numpyro.sample(f"n2_{ps}",  dist.Uniform(-6., -5.))
-            sb1 = numpyro.sample(f"sb1_{ps}", dist.Uniform(5., 40.0))
-
-            theta_tmp = jnp.array([1., n1, n2, sb1])
-            s_ary = jnp.logspace(-1., 2., 1000)
-            dnds_ary = dnds(s_ary, theta_tmp)
-            A = Sps / jnp.trapz(s_ary * dnds_ary, s_ary)
-            theta.append([A, n1, n2, sb1])
+            A   = numpyro.sample(f"A_{ps}",   dist.Uniform(1e-4, 1.))
+            n1  = numpyro.sample(f"n1_{ps}",  dist.Uniform(3., 7.))
+            n2  = numpyro.sample(f"n2_{ps}",  dist.Uniform(-7., -3.))
+            sb  = numpyro.sample(f"sb_{ps}",  dist.Uniform(5., 20.))
+            theta.append([A, n1, n2, sb])
         theta = jnp.array(theta)
 
         
@@ -166,7 +167,53 @@ class NPModelCMP:
         theta = repeat(theta, "n_ps n_param -> n_exp n_ps n_param", n_exp=len(expreg_indices))
         theta = theta.at[:, :, 0].set(theta[:, :, 0] / exposure_multiplier[:, None])
         theta = theta.at[:, :, -1].set(theta[:, :, -1] * exposure_multiplier[:, None])
-        theta = theta.at[:, :, -2].set(theta[:, :, -2] * exposure_multiplier[:, None])
+        # theta = theta.at[:, :, -2].set(theta[:, :, -2] * exposure_multiplier[:, None])
+        
+        with numpyro.plate("data", size=len(mu[~self.mask_roi]), dim=-1):
+            
+            log_like_exp = log_like_np_exp_vmapped(theta, mu_batch, npt_compressed_batch, data_batch, self.f_ary, self.df_rho_ary, self.k_max, len(expreg_indices[0]))
+            loglike = jnp.concatenate(log_like_exp)[:len(mu[~self.mask_roi])]
+
+            with handlers.mask(mask=~jnp.logical_or(jnp.isinf(loglike), jnp.isnan(loglike))):
+                return numpyro.factor('log-likelihood', loglike)
+
+    def model_ps(self):
+        
+        mu = jnp.zeros_like(self.data)
+
+        # np templates
+        npt_compressed = jnp.array([self.nfw_1p2])
+        theta = []
+        for ips, ps in enumerate(["nfw"]):
+            A   = numpyro.sample(f"A_{ps}",   dist.Uniform(1e-4, 1.))
+            n1  = numpyro.sample(f"n1_{ps}",  dist.Uniform(3., 7.))
+            n2  = numpyro.sample(f"n2_{ps}",  dist.Uniform(-7., -3.))
+            sb  = numpyro.sample(f"sb_{ps}",  dist.Uniform(5., 20.))
+            theta.append([A, n1, n2, sb])
+        theta = jnp.array(theta)
+
+        
+        # likelihoods and exposure
+        # Pad the last exposure region so that all are the same size
+        exp_lens = [len(self.expreg_indices[i]) for i in range(len(self.expreg_indices))]
+        n_pad = exp_lens[0] - exp_lens[-1]
+        
+        expreg_indices = jnp.zeros_like(self.expreg_indices)
+        expreg_indices = expreg_indices.at[:-1].set(self.expreg_indices[:-1])
+        expreg_indices = expreg_indices.at[-1].set(jnp.pad(self.expreg_indices[-1], (0, n_pad)))
+
+        log_like_np_exp_vmapped = jax.vmap(log_like_np, in_axes=(0, 0, 1, 0, None, None, None, None))
+                
+        # Get relevant arrays for different exposure regions
+        mu_batch = mu[~self.mask_roi][jnp.array(expreg_indices)]
+        npt_compressed_batch = npt_compressed[:, ~self.mask_roi][:, jnp.array(expreg_indices)]
+        data_batch = self.data[~self.mask_roi][jnp.array(expreg_indices)]
+        exposure_multiplier = self.exposure_means_list / self.exposure_mean
+        
+        theta = repeat(theta, "n_ps n_param -> n_exp n_ps n_param", n_exp=len(expreg_indices))
+        theta = theta.at[:, :, 0].set(theta[:, :, 0] / exposure_multiplier[:, None])
+        theta = theta.at[:, :, -1].set(theta[:, :, -1] * exposure_multiplier[:, None])
+        # theta = theta.at[:, :, -2].set(theta[:, :, -2] * exposure_multiplier[:, None])
         
         with numpyro.plate("data", size=len(mu[~self.mask_roi]), dim=-1):
             
@@ -177,15 +224,22 @@ class NPModelCMP:
                 return numpyro.factor('log-likelihood', loglike)
 
 
-    def fit_svi(self, n_steps=10000, lr=5e-3):
+    def fit_svi(self, model_name='model', n_steps=10000, lr=5e-3):
+
+        if model_name == 'model':
+            model = self.model
+        elif model_name == 'model_ps':
+            model = self.model_ps
+        else:
+            raise ValueError("model_name not recognized")
 
         self.guide = autoguide.AutoIAFNormal(
-            self.model, num_flows=5, hidden_dims=[128, 128], nonlinearity=stax.Tanh
+            model, num_flows=5, hidden_dims=[128, 128], nonlinearity=stax.Tanh
         )
         optimizer = optim.optax_to_numpyro(optax.chain(optax.clip(1.), optax.adam(lr)))
         loss = Trace_ELBO(num_particles=8, vectorize_particles=True)
 
-        self.svi = SVI(self.model, self.guide, optimizer, loss)
+        self.svi = SVI(model, self.guide, optimizer, loss)
         self.svi_results = self.svi.run(jax.random.PRNGKey(42), n_steps)
         return self.svi_results
 
@@ -201,9 +255,16 @@ class NPModelCMP:
         return self.svi_samples
 
 
-    def run_nuts(self, num_chains=4, num_warmup=500, num_samples=5000, step_size=0.1, **model_static_kwargs):
+    def run_nuts(self, model_name='model', num_chains=4, num_warmup=500, num_samples=5000, step_size=0.1, **model_static_kwargs):
         
-        kernel = NUTS(self.model, max_tree_depth=4, dense_mass=False, step_size=step_size)
+        if model_name == 'model':
+            model = self.model
+        elif model_name == 'model_ps':
+            model = self.model_ps
+        else:
+            raise ValueError("model_name not recognized")
+        
+        kernel = NUTS(model, max_tree_depth=4, dense_mass=False, step_size=step_size)
         self.nuts_mcmc = MCMC(kernel, num_warmup=num_warmup, num_samples=num_samples, num_chains=num_chains, chain_method='vectorized')
         self.nuts_mcmc.run(jax.random.PRNGKey(42), **model_static_kwargs)
         

src/fpp/simulations/wrapper.py
@@ -233,7 +233,7 @@ def simulator_for_model_p6v11(m, vd, sim_all=False, delta_psf=False, flat_exposu
 
 
 
-def simulator_for_cmp(m, vd):
+def simulator_for_cmp(m, vd, **kwargs):
     """Wrapper for simulator function.
 
     Args:
@@ -268,7 +268,7 @@ def simulator_for_cmp(m, vd):
 
     mask_normalize_counts = np.array(m.normalization_mask)
     mask_roi = np.array(m.mask_roi)
-    mask_sim = mask_normalize_counts
+    mask_sim = np.zeros_like(m.data, dtype=bool)
 
     kp = KingPSF()
     psf_r_func = lambda r: kp.psf_fermi_r(r)