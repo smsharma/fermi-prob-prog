4663a1d242b16d2e7694dc42bdd01192494e322d
Yitian Sun <yitian.sun.26@gmail.com>
2026-03-06 01:51:10 -0500

    custom fit_xxx.py

---
+++ analysis/fit_fermi.py
@@ -0,0 +1,59 @@
+import os
+import sys
+import dill as pickle
+import argparse
+
+import numpy as np
+import healpy as hp
+
+import jax
+jax.config.update("jax_enable_x64", True)
+import jax.numpy as jnp
+
+wdir = "/n/home07/yitians/fermi/fermi-prob-prog/analysis"
+from fpp.models.np_model import NPModel
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', type=int) # 0-7
+    args = parser.parse_args()
+
+    fit_list = ['svi', 'hmc']
+    dif_list = [["ModelO"], ["ModelA"], ["ModelF"], ["ModelO", "ModelA", "ModelF"]]
+    i_fit, i_dif = np.unravel_index(args.i, (len(fit_list), len(dif_list)))
+    fit = fit_list[i_fit]
+    dif = dif_list[i_dif]
+    subname = fit + '-' + ['O', 'A', 'F', 'OAF'][i_dif]
+    print(f"Running {subname} ...")
+
+    save_dir = f"{wdir}/../outputs/production/fits/p6v11oaf"
+    os.makedirs(save_dir, exist_ok=True)
+
+    data = jnp.array(np.load(f"../outputs/production/simulations/23new_p6v11.npy")[0], dtype=jnp.int32)
+    
+    m = NPModel(
+        data=data,
+        psf_tag='king',
+        n_exp=7,
+        diffuse_names=dif,
+    )
+
+    if fit == 'svi':
+        m.fit_svi(
+            data=data, rng_key=jax.random.PRNGKey(42),
+            n_steps=10000, lr=3e-4,
+            guide='iaf', num_flows=5, hidden_dims=[128, 128],
+            num_particles=16,
+        )
+        samples = m.get_svi_samples(num_samples=50000)
+
+    elif fit == 'hmc':
+        m.run_nuts(
+            data=data, rng_key=jax.random.PRNGKey(42),
+            num_chains=4, num_warmup=1000, num_samples=10000//4, step_size=0.05,
+        )
+        samples = m.nuts_mcmc.get_samples()
+    
+    pickle.dump(samples, open(f"{save_dir}/{subname}.p", 'wb'))
--- analysis/submit.sh
+++ analysis/submit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name=hmc_Dnmold_deltapsf_Mbase_deltapsf
-#SBATCH --array=0-29
-#SBATCH --partition=gpu
+#SBATCH --job-name=oaf
+#SBATCH --array=4-7
+#SBATCH --partition=iaifi_gpu
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=64GB
-#SBATCH --time=0-03:00:00
+#SBATCH --time=0-05:00:00
 #SBATCH --output=/n/home07/yitians/fermi/fermi-prob-prog/outputs/slurm/%x_%a.out
 #SBATCH --error=/n/home07/yitians/fermi/fermi-prob-prog/outputs/slurm/%x_%a.err
 #SBATCH --account=iaifi_lab
@@ -18,13 +18,15 @@ source /n/home07/yitians/setup/torch.sh
 
 cd /n/home07/yitians/fermi/fermi-prob-prog/analysis
 
+python fit_oaf.py -i $SLURM_ARRAY_TASK_ID
+
 #===== np =====
 # python fit.py -i $SLURM_ARRAY_TASK_ID --fit_type svi --n_step 10000 -n 50000 \
-#    --data nmold_deltapsf --model base_deltapsf --n_exp 7 --seed $((SLURM_ARRAY_TASK_ID * 4224)) --lr 3e-4 --n_par 16 \
+#    --data fermi --model base --n_exp 7 --seed $((SLURM_ARRAY_TASK_ID * 4224)) --lr 3e-4 --n_par 16 \
 #    --guide iaf --num_flows 5 --hidden_dim_n 128 --renyi_alpha 1 --comment ""
 
-python fit.py -i $SLURM_ARRAY_TASK_ID --fit_type hmc --n_step 0     -n 10000 \
-    --data nmold_deltapsf --model base_deltapsf --n_exp 7 --seed $((SLURM_ARRAY_TASK_ID * 4224)) --comment ""
+# python fit.py -i $SLURM_ARRAY_TASK_ID --fit_type hmc --n_step 0     -n 25000 \
+#     --data fermi --model base --n_exp 7 --seed $((SLURM_ARRAY_TASK_ID * 4224)) --comment ""
 
 #===== pois =====
 # python fit.py -i $SLURM_ARRAY_TASK_ID --fit_type svi --n_step 10000 -n 50000 \
